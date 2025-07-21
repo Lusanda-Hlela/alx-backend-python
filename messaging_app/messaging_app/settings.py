0c87ecd6205e010695d9f99ecdbdcb0465cd77e5
lusanda <hlelalusa@gmail.com>
2025-07-21 22:26:37 +0200

    Task 0 Version 2

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -142,8 +142,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
+
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
